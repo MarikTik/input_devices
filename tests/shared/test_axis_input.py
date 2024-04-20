97e6f4468f089c63a7ac16691c213a43fb79d140
MarikTik <mtik.philosopher@gmail.com>
2024-04-19 20:59:18 -0700

    added adjust method test for AxisInput

--- tests/shared/test_axis_input.py
+++ tests/shared/test_axis_input.py
@@ -44,6 +44,21 @@ def test_inverted_get(cartesian_axis_input_inverted):
      assert input.get_x() == 50
      assert input.get_y() == 20
 
+def test_adjusted_get(cartesian_axis_input):
+     input: CartesianAxisInput = copy(cartesian_axis_input)
+     input._set_x(1000)
+     input._set_y(1000)
+     assert input.get_adjusted_x((-3000, 3000), 0) == 0 # testing blindspot ignore mode
+     assert input.get_adjusted_y((-3000, 3000), 0) == 0
+     input._set_x(-3001)
+     input._set_y(3001)
+     assert 0 > input.get_adjusted_x((-3000, 3000), 0) > -3 #estimated value after blindspot range adjustment
+     assert 0 < input.get_adjusted_y((-3000, 3000), 0) < 3
+     input._set_x(10000)
+     input._set_y(-10000)
+     assert input.get_adjusted_x((-3000, 3000), axis_zero=1000) - 8471 < 1 # testing zero shift
+     assert input.get_adjusted_y((-3000, 3000), axis_zero=-1000) + 8471 < 1
+
 
 
 
