b71ff16412e9d7ce30cbc5a427049c0550683c98
MarikTik <mtik.philosopher@gmail.com>
2024-04-19 20:37:32 -0700

    added tests for AxisInput get_[x/y] methods

--- tests/shared/test_axis_input.py
+++ tests/shared/test_axis_input.py
@@ -1,30 +1,50 @@
-from src.shared import CartesianAxisInput, HorizontalAxisInput, VerticalAxisInput
-from pytest import fixture
+from src.shared import CartesianAxisInput
+import pytest
+from copy import copy
 
-@fixture
-def sample_horizontal_axis_input():
-     return HorizontalAxisInput(custom_bounds=(-1, 1), zero=0, axis_inverted=False, blindspot_range=(-0.3, 0.3))
+default_value_range = (-32768, 32767)
 
-@fixture
-def sample_vertical_axis_input():
-     return VerticalAxisInput(custom_bounds=(-1, 1), zero=0, axis_inverted=False, blindspot_range=(-0.3, 0.3))
+@pytest.fixture
+def cartesian_axis_input():
+     return CartesianAxisInput(
+          horizontal_value_range=default_value_range, 
+          vertical_value_range=default_value_range, 
+          horizontal_axis_inverted=False, 
+          vertical_axis_inverted=False
+     )
+
+@pytest.fixture
+def cartesian_axis_input_inverted():
+     return CartesianAxisInput(
+          horizontal_value_range=default_value_range,
+          vertical_value_range=default_value_range,
+          horizontal_axis_inverted=True,
+          vertical_axis_inverted=True
+          )
+
+def test_regular_get(cartesian_axis_input):
+     input: CartesianAxisInput = copy(cartesian_axis_input)
+     input._set_x(100)
+     input._set_y(30)
+     assert input.get_x() == 100
+     assert input.get_y() == 30
+     input._set_x(-50)
+     input._set_y(-20)
+     assert input.get_x() == -50
+     assert input.get_y() == -20
+
+def test_inverted_get(cartesian_axis_input_inverted):
+     input: CartesianAxisInput = copy(cartesian_axis_input_inverted)
+     input._set_x(100)
+     input._set_y(30)
+     assert input.get_x() == -100
+     assert input.get_y() == -30
+     input._set_x(-50)
+     input._set_y(-20)
+     assert input.get_x() == 50
+     assert input.get_y() == 20
+
+
 
 
  
-
-def test_horizontal_range_general(sample_horizontal_axis_input):
-     axis_input: HorizontalAxisInput = sample_horizontal_axis_input
-
-     axis_input._x = -1
-     assert axis_input.get_x() == -1.0
-     axis_input._x = 1
-     assert axis_input.get_x() == 1.0
-
-def test_horizontal_range_blindspot(sample_horizontal_axis_input):
-     axis_input: HorizontalAxisInput = sample_horizontal_axis_input
-     axis_input._x = 0
-     assert axis_input.get_x_adjusted() == 0
-     axis_input._x = 0.2
-     assert axis_input.get_x_adjusted() == 0
-     axis_input._x = -0.2
-     assert axis_input.get_x_adjusted() == 0
